86b3113930a73fc2df09ff3763052d21c7dc2e58
Anurag Patbandha <anurag.patbandha@dreamorbit.com>
2018-03-30 16:24:43 +0530

    ngx table

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -28,7 +28,7 @@ export class LoginComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.loaderService.display(false);
+   
     this.loginBlock = true;
     // var rememberUser = JSON.parse(localStorage.getItem('rememberUser'));
     
