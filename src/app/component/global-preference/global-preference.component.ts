bae2b7c9f0cb0bb1f31b20bd205683bdc3fe02f4
Ganeshmurthy1 <ganeshmurthy.p@gmail.com>
2018-04-02 21:08:20 +0530

    dropdown

--- src/app/component/global-preference/global-preference.component.ts
+++ src/app/component/global-preference/global-preference.component.ts
@@ -51,8 +51,7 @@ import { GlobalPreferencesService } from '../../services/global-preferences.serv
 import { LoaderService } from './../../services/loader.service';
 import { Response } from '@angular/http';
 import { Router} from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal';
- 
+import { BsModalService } from 'ngx-bootstrap/modal'; 
 declare var $: any;
 
  
@@ -107,55 +106,63 @@ export class GlobalPreferenceComponent implements OnInit {
 
 //     });
 // });
+
+
+  }
+
+  ngAfterViewInit(){
+    setTimeout(()=>{     
+      $(document).ready(function () {
  
-$(document).ready(function () {
-// Select the main list and add the class "hasSubmenu" in each LI that contains an UL
-$('ul').each(function(){
- 
-  $(this).find("li").has("ul").addClass("hasSubmenu");
-});
-// Find the last li in each level
-$('li:last-child').each(function(){
- 
-  // Check if LI has children
-  if ( $(this).children('ul').length === 0){
-    // Add border-left in every UL where the last LI has not children
-    $(this).closest('ul').css("border-left", "1px solid gray");
-  } else {
-    // Add border in child LI, except in the last one
-    $(this).closest('ul').children("li").not(":last").css("border-left","1px solid gray");
-    // Add the class "addBorderBefore" to create the pseudo-element :defore in the last li
-    $(this).closest('ul').children("li").last().children("a").addClass("addBorderBefore");
-    // Add margin in the first level of the list
-    $(this).closest('ul').css("margin-top","20px");
-    // Add margin in other levels of the list
-    $(this).closest('ul').find("li").children("ul").css("margin-top","20px");
-  };
-});
-// Add bold in li and levels above
-$('ul li').each(function(){
+  // Select the main list and add the class hasSubmenu" in each LI that contains an UL
+  $('ul').each(function(){  
+    
+    $(this).find("li").has("ul").addClass("hasSubmenu");
+  });
+  // Find the last li in each level
+  $('li:last-child').each(function(){ 
+    // Check if LI has children
+    if ( $(this).children('ul').length === 0){
+
+      // Add border-left in every UL where the last LI has not children
+      $(this).closest('ul').css("border-left", "1px solid gray");
+    } else {
+      // Add border in child LI, except in the last one
+      $(this).closest('ul').children("li").not(":last").css("border-left","1px solid gray");
+      // Add the class "addBorderBefore" to create the pseudo-element :defore in the last li
+      $(this).closest('ul').children("li").last().children("a").addClass("addBorderBefore");
+      // Add margin in the first level of the list
+      $(this).closest('ul').css("margin-top","20px");
+      // Add margin in other levels of the list
+      $(this).closest('ul').find("li").children("ul").css("margin-top","20px");
+    };
+  });
+  // Add bold in li and levels above
+  $('ul li').each(function(){
+     
+    $(this).mouseenter(function(){
+      $( this ).children("a").css({"font-weight":"bold","color":"#336b9b"});
+    });
+    $(this).mouseleave(function(){
+      $( this ).children("a").css({"font-weight":"normal","color":"#428bca"});
+    });
+  });
+  // Add button to expand and condense - Using FontAwesome
+  $('ul li.hasSubmenu').each(function(){
+    
+    $(this).prepend("<a href='#'><i class='fa fa-minus-circle'></i><i style='display:none;' class='fa fa-plus-circle'></i></a>");
+    $(this).children("a").not(":last").removeClass().addClass("toogle");
+  });
+  // Actions to expand and consense
+  $('ul li.hasSubmenu a.toogle').click(function(){
+     
+    $(this).closest("li").children("ul").toggle("slow");
+    $(this).children("i").toggle();
+    return false;
+  });
+    });
+ },3000);    
    
-  $(this).mouseenter(function(){
-    $( this ).children("a").css({"font-weight":"bold","color":"#336b9b"});
-  });
-  $(this).mouseleave(function(){
-    $( this ).children("a").css({"font-weight":"normal","color":"#428bca"});
-  });
-});
-// Add button to expand and condense - Using FontAwesome
-$('ul li.hasSubmenu').each(function(){
-  
-  $(this).prepend("<a href='#'><i class='fa fa-minus-circle'></i><i style='display:none;' class='fa fa-plus-circle'></i></a>");
-  $(this).children("a").not(":last").removeClass().addClass("toogle");
-});
-// Actions to expand and consense
-$('ul li.hasSubmenu a.toogle').click(function(){
-   
-  $(this).closest("li").children("ul").toggle("slow");
-  $(this).children("i").toggle();
-  return false;
-});
-  });
 
   }
   getAllGlobalPreferences(){
@@ -171,24 +178,29 @@ $('ul li.hasSubmenu a.toogle').click(function(){
       console.log("this.allEventCluster",this.allEventCluster);
     })
   }
-  parentCheckboxClicked(indx){
-console.log("index",indx);
+//   parentCheckboxClicked(indx){
+// console.log("index",indx);
 
-// this.providers.forEach(providerElement => {
-//   this.allEventCluster.forEach(clusterElement => {
-//     if(providerElement.levelId == clusterElement.providerId){
-//       this.sortedClusterArray.push(clusterElement.providerId);      
-//     }          
-//   });
+// // this.providers.forEach(providerElement => {
+// //   this.allEventCluster.forEach(clusterElement => {
+// //     if(providerElement.levelId == clusterElement.providerId){
+// //       this.sortedClusterArray.push(clusterElement.providerId);      
+// //     }          
+// //   });
+// // });
+// this.allEventCluster.forEach(clusterElement => {
+//   if(clusterElement.providerId == indx){
+//     this.sortedClusterArray.push(clusterElement); 
+//     console.log('this.sortedClusterArray',this.sortedClusterArray);
+//   }
 // });
-this.allEventCluster.forEach(clusterElement => {
-  if(clusterElement.providerId == indx){
-    this.sortedClusterArray.push(clusterElement); 
-    console.log('this.sortedClusterArray',this.sortedClusterArray);
-  }
-});
-  }
-
- 
+//   }
+providerClicked(provdrId){
+  console.log("provdrId",provdrId);
+}
+clusterClicked(provdrId,clusterId){
+  console.log("provdrId",provdrId);
+  console.log("clusterId",clusterId);
+}
 }
 
